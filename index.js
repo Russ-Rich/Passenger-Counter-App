c5a20aedd1e4220dcdbaf3eb4d94315e90b53751
Russ <russ3750@gmail.com>
2023-08-25 09:22:04 -0500

    Update node modules

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.getElementById("count-el").innerText = 5;
+//document.getElementById("count-el").innerText = 5;
+
+let count = 50
+count = count + 50
+count = count - 75
+count = count + 20
+
+console.log(count)
