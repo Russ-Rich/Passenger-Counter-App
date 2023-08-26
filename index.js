5554d4c39dc2219f57a8bf85a5e5ca3bc67db81c
Russ <russ3750@gmail.com>
2023-08-26 09:30:59 -0500

    Finished

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-//document.getElementById("count-el").innerText = 5;
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
 
-let count = 50
+console.log(saveEl);
 
+function increment() {
+	count += 1;
+	countEl.innerText = count;
+}
 
-console.log(count)
+function save() {
+	let countStr = count + " - ";
+	saveEl.textContent += countStr;
+	countEl.textContent = 0;
+	count = 0;
+	console.log(count);
+}
